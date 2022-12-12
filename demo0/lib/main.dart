8e3a7a839098ed86689cf300898e27b69200f551
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-12 11:27:52 +0700

    1234

--- demo0/lib/main.dart
+++ demo0/lib/main.dart
@@ -1,34 +1,7 @@
+import 'package:demo0/src/app.dart';
 import 'package:demo0/src/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: HomePage(),
-    );
-  }
-
-  _buildHome() {
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: Text(
-          "CodeMobiles",
-          style: TextStyle(color: Colors.black),
-        ),
-      ),
-    );
-
-  }
+  runApp(const CMApp());
 }

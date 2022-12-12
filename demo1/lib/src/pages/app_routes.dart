eb93cde2044575e9ce2ac2de445926ba7ef63885
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-12 13:13:12 +0700

    1234

---
+++ demo1/lib/src/pages/app_routes.dart
@@ -0,0 +1,13 @@
+class AppRoute {
+  static const home = 'home';
+  static const login = 'login';
+  static const management = 'management';
+  static const map = 'map';
+
+  static get all => <String, WidgetBuilder>{
+        login: (context) => const LoginPage(), // demo how to used widget
+        home: (context) => const HomePage(),
+        management: (context) => const ManagementPage(),
+        map: (context) => const MapPage(),
+      };
+}

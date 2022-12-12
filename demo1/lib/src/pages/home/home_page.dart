7af10f2a6b863572d98cea5ea5c281157554bf04
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-12 11:54:04 +0700

    1234

--- demo1/lib/src/pages/home/home_page.dart
+++ demo1/lib/src/pages/home/home_page.dart
@@ -12,7 +12,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('appbarTitle'),
+          title: Text('HomePage'),
         ),
         body: Text("HomePage"));
   }

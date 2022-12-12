9b8271964d4bf8d3cd82ba86fb92264a7c01b68d
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-12 11:58:38 +0700

    1234

---
+++ demo1/lib/src/pages/management/management_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ManagementPage extends StatefulWidget {
+  const ManagementPage({Key? key}) : super(key: key);
+
+  @override
+  State<ManagementPage> createState() => _ManagementPageState();
+}
+
+class _ManagementPageState extends State<ManagementPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('ManagementPage'),
+        ),
+        body: Text("ManagementPage"));
+  }
+}

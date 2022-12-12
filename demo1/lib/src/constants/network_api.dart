cb7a38b34f9d406b22da216ffdadd7d6ae97f029
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-12 14:07:03 +0700

    1234

---
+++ demo1/lib/src/constants/network_api.dart
@@ -0,0 +1,8 @@
+// https://cmcrud.herokuapp.com/products
+
+class NetworkAPI {
+  static const String baseURL = 'http://192.168.0.178:1150';
+  // static const String baseURL = 'https://cmcrud.herokuapp.com'; // 'https://cmcrud.herokuapp.com'
+  static const String imageURL = '$baseURL/images';
+  static const String product = '/products';
+}

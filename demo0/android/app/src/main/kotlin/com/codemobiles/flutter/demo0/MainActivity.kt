1a7b6b5d8f0fc186ac6ce4083dde19a1e8300eb1
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-12 09:23:06 +0700

    1234

---
+++ demo0/android/app/src/main/kotlin/com/codemobiles/flutter/demo0/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.codemobiles.flutter.demo0
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- note.md
+++ note.md
@@ -1,3 +1,3 @@
 # Preparation
 
-1234
+- master and doc: https://drive.google.com/file/d/1cbhx4dtI_KcEXbjDorhjm6n114gGBK_8/view?usp=share_link

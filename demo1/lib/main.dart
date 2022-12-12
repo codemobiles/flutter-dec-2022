b3a86d4f822293a30f590860fcbad698f89114a6
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-12 11:39:50 +0700

    1234

--- demo1/lib/main.dart
+++ demo1/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:demo1/src/app.dart';
 import 'package:flutter/cupertino.dart';
 
 void main(){
---
+++ demo1/lib/src/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CMApp extends StatelessWidget {
+  const CMApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

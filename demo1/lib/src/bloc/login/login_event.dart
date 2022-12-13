9dc04582ff026a61c06b40325745a4812bf0f667
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-13 11:02:25 +0700

    1234

--- demo1/lib/src/bloc/login/login_event.dart
+++ demo1/lib/src/bloc/login/login_event.dart
@@ -11,3 +11,21 @@ class LoginEvent_Login extends LoginEvent{
   final User payload;
   LoginEvent_Login(this.payload);
 }
+
+// Register Event
+class LoginEvent_Register extends LoginEvent{
+  final User payload;
+  LoginEvent_Register(this.payload);
+}
+
+// Forgot Event
+class LoginEvent_Forgot extends LoginEvent{
+  final String username;
+  LoginEvent_Forgot(this.username);
+}
+
+
+// Logout Event
+class LoginEvent_Logout extends LoginEvent{
+  LoginEvent_Logout();
+}

e917c4628b98e29a44c0b12bc96fc33fb401dd2e
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-13 11:26:36 +0700

    1234

--- demo1/lib/src/bloc/login/login_bloc.dart
+++ demo1/lib/src/bloc/login/login_bloc.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:demo1/src/constants/app_setting.dart';
 import 'package:demo1/src/models/user.dart';
 import 'package:equatable/equatable.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 part 'login_event.dart';
 
@@ -11,12 +13,26 @@ part 'login_state.dart';
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
   LoginBloc() : super(LoginState()) {
     // Login
-    on<LoginEvent_Login>((event, emit) {
+    on<LoginEvent_Login>((event, emit) async {
       final String username = event.payload.username;
       final String password = event.payload.password;
 
       if (username == 'admin' && password == '1234') {
+          // SharedPreferences prefs;
+          //
+          // SharedPreferences.getInstance().then((value)  {
+          //   SharedPreferences.getInstance().then((value)  {
+          //     SharedPreferences.getInstance().then((value)  {
+          //       SharedPreferences.getInstance().then((value)  {
+          //         prefs = value;
+          //       });
+          //     });
+          //   });
+          // });
 
+        final prefs = await SharedPreferences.getInstance();
+        await prefs.setString(AppSetting.token, 'TExkgk0494oksrkf');
+        await prefs.setString(AppSetting.username, username);
 
       }
 

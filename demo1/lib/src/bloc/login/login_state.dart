b03437db2563d88c67b946ccedf32738dca1073d
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-13 11:05:19 +0700

    1234

--- demo1/lib/src/bloc/login/login_state.dart
+++ demo1/lib/src/bloc/login/login_state.dart
@@ -6,6 +6,10 @@ class LoginState extends Equatable {
   final LoginStatus status;
   const LoginState({this.status = LoginStatus.init});
 
+  LoginState copyWith({LoginStatus? status}){
+    return LoginState(status: status ?? this.status);
+  }
+
   @override
-  List<Object> get props => [];
+  List<Object> get props => [status];
 }

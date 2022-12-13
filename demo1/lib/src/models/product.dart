8029c9cb7e7cff1220d761f778e2e60093ede748
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-13 10:41:14 +0700

    1234

---
+++ demo1/lib/src/models/product.dart
@@ -0,0 +1,56 @@
+// To parse this JSON data, do
+//
+//     final product = productFromJson(jsonString);
+
+import 'dart:convert';
+
+import 'package:equatable/equatable.dart';
+
+List<Product> productFromJson(String str) => List<Product>.from(json.decode(str).map((x) => Product.fromJson(x)));
+
+String productToJson(List<Product> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Product extends Equatable{
+  Product({
+    this.id,
+    required this.name,
+    this.image,
+    required this.stock,
+    required this.price,
+    this.createdAt,
+    this.updatedAt,
+  });
+
+  int? id;
+  String name;
+  String? image;
+  int stock;
+  int price;
+  DateTime? createdAt;
+  DateTime? updatedAt;
+
+  factory Product.fromJson(Map<String, dynamic> json) => Product(
+    id: json["id"],
+    name: json["name"],
+    image: json["image"],
+    stock: json["stock"],
+    price: json["price"],
+    createdAt: DateTime.parse(json["created_at"]),
+    updatedAt: DateTime.parse(json["updated_at"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "name": name,
+    "image": image,
+    "stock": stock,
+    "price": price,
+    "created_at": createdAt?.toIso8601String(),
+    "updated_at": updatedAt?.toIso8601String(),
+  };
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [id, name, image, stock, price, createdAt, updatedAt];
+
+}

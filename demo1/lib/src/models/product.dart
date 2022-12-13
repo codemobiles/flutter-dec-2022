6080d7f536a6e8a5b6281f28abe984689024815a
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-13 15:49:25 +0700

    1234

--- demo1/lib/src/models/product.dart
+++ demo1/lib/src/models/product.dart
@@ -4,53 +4,46 @@
 
 import 'dart:convert';
 
-import 'package:equatable/equatable.dart';
-
 List<Product> productFromJson(String str) => List<Product>.from(json.decode(str).map((x) => Product.fromJson(x)));
 
 String productToJson(List<Product> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
-class Product extends Equatable{
+class Product {
   Product({
     this.id,
     required this.name,
-    this.image,
+    required this.image,
     required this.stock,
     required this.price,
     this.createdAt,
     this.updatedAt,
   });
 
-  int? id;
-  String name;
-  String? image;
-  int stock;
-  int price;
-  DateTime? createdAt;
-  DateTime? updatedAt;
+  final int? id;
+  final String name;
+  final String image;
+  final int stock;
+  final int price;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
 
   factory Product.fromJson(Map<String, dynamic> json) => Product(
-    id: json["id"],
-    name: json["name"],
-    image: json["image"],
-    stock: json["stock"],
-    price: json["price"],
-    createdAt: DateTime.parse(json["created_at"]),
-    updatedAt: DateTime.parse(json["updated_at"]),
+    id: json["id"] == null ? null : json["id"],
+    name: json["name"] == null ? null : json["name"],
+    image: json["image"] == null ? null : json["image"],
+    stock: json["stock"] == null ? null : json["stock"],
+    price: json["price"] == null ? null : json["price"],
+    createdAt: json["created_at"] == null ? null : DateTime.parse(json["created_at"]),
+    updatedAt: json["updated_at"] == null ? null : DateTime.parse(json["updated_at"]),
   );
 
   Map<String, dynamic> toJson() => {
-    "id": id,
-    "name": name,
-    "image": image,
-    "stock": stock,
-    "price": price,
-    "created_at": createdAt?.toIso8601String(),
-    "updated_at": updatedAt?.toIso8601String(),
+    "id": id == null ? null : id,
+    "name": name == null ? null : name,
+    "image": image == null ? null : image,
+    "stock": stock == null ? null : stock,
+    "price": price == null ? null : price,
+    "created_at": createdAt == null ? null : createdAt?.toIso8601String(),
+    "updated_at": updatedAt == null ? null : updatedAt?.toIso8601String(),
   };
-
-  @override
-  // TODO: implement props
-  List<Object?> get props => [id, name, image, stock, price, createdAt, updatedAt];
-
 }

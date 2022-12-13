bb14db0ed4d824bf021fb29a32eeb38f3fa6309a
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-13 09:26:42 +0700

    1234

--- demo1/lib/src/app.dart
+++ demo1/lib/src/app.dart
@@ -1,3 +1,4 @@
+import 'package:demo1/src/bloc/home_bloc.dart';
 import 'package:demo1/src/bloc/login/login_bloc.dart';
 import 'package:demo1/src/pages/app_routes.dart';
 import 'package:flutter/material.dart';
@@ -12,9 +13,10 @@ class CMApp extends StatelessWidget {
 
     // Provider vs Builder
     final loginBloc = BlocProvider<LoginBloc>(create: (context)=>LoginBloc());
+    final homeBloc = BlocProvider<HomeBloc>(create: (context)=>HomeBloc());
 
     return MultiBlocProvider(
-      providers: [loginBloc],
+      providers: [loginBloc, homeBloc],
       child: MaterialApp(
         title: "CMApp",
         home: LoginPage(),

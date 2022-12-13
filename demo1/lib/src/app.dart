80ac201fae893463125552b71ceb6a0d802d12a1
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-13 13:30:11 +0700

    1234

--- demo1/lib/src/app.dart
+++ demo1/lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:demo1/src/bloc/home/home_bloc.dart';
 import 'package:demo1/src/bloc/login/login_bloc.dart';
+import 'package:demo1/src/constants/app_setting.dart';
 import 'package:demo1/src/pages/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -8,16 +9,14 @@ import 'pages/pages.dart';
 
 final navigatorState = GlobalKey<NavigatorState>();
 
-
 class CMApp extends StatelessWidget {
   const CMApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     // Provider vs Builder
-    final loginBloc = BlocProvider<LoginBloc>(create: (context)=>LoginBloc());
-    final homeBloc = BlocProvider<HomeBloc>(create: (context)=>HomeBloc());
+    final loginBloc = BlocProvider<LoginBloc>(create: (context) => LoginBloc());
+    final homeBloc = BlocProvider<HomeBloc>(create: (context) => HomeBloc());
 
     return MultiBlocProvider(
       providers: [loginBloc, homeBloc],
@@ -30,11 +29,22 @@ class CMApp extends StatelessWidget {
     );
   }
 
-  _initialPage()  {
+  _initialPage() {
     return FutureBuilder(
       future: SharedPreferences.getInstance(),
-      builder: (context, snapshot){
-        return LoginPage();
+      builder: (context, snapshot) {
+        if (snapshot.hasData == false) {
+          // Loading Page
+          return Center(
+              child: Text(
+            "Loading",
+            style: TextStyle(color: Colors.white),
+          ));
+        }
+        
+        final token = snapshot.data!.getString(AppSetting.token) ?? "";
+
+        return token.isEmpty ? LoginPage() : HomePage();
       },
     );
   }

63b2818d1aad50cdbfe5c33a6a21dbd4a6151156
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-13 11:32:13 +0700

    1234

--- demo1/lib/src/bloc/login/login_bloc.dart
+++ demo1/lib/src/bloc/login/login_bloc.dart
@@ -14,26 +14,21 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
   LoginBloc() : super(LoginState()) {
     // Login
     on<LoginEvent_Login>((event, emit) async {
+
+      emit(state.copyWith(status: LoginStatus.fetching));
+      // Simulate delay
+      await Future.delayed(Duration(seconds: 2));
+
       final String username = event.payload.username;
       final String password = event.payload.password;
 
       if (username == 'admin' && password == '1234') {
-          // SharedPreferences prefs;
-          //
-          // SharedPreferences.getInstance().then((value)  {
-          //   SharedPreferences.getInstance().then((value)  {
-          //     SharedPreferences.getInstance().then((value)  {
-          //       SharedPreferences.getInstance().then((value)  {
-          //         prefs = value;
-          //       });
-          //     });
-          //   });
-          // });
-
         final prefs = await SharedPreferences.getInstance();
         await prefs.setString(AppSetting.token, 'TExkgk0494oksrkf');
         await prefs.setString(AppSetting.username, username);
-
+        emit(state.copyWith(status: LoginStatus.success));
+      }else{
+        emit(state.copyWith(status: LoginStatus.failed));
       }
 
     });

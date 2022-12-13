c90761adbe25d25bc713b1aab8a1d9593b87f313
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-13 09:43:01 +0700

    1234

--- demo1/lib/src/bloc/home/home_state.dart
+++ demo1/lib/src/bloc/home/home_state.dart
@@ -1,8 +1,23 @@
 part of 'home_bloc.dart';
 
-
 class HomeState extends Equatable {
-  const HomeState();
+  final int count1;
+  final int count2;
+
+  const HomeState({
+    this.count1 = 0,
+    this.count2 = 0,
+  });
+
+  HomeState copyWith({
+    int? count1,
+    int? count2,
+  }) {
+    return HomeState(
+      count1: count1 ?? this.count1,
+      count2: count2 ?? this.count2,
+    );
+  }
 
   @override
   List<Object> get props => [];

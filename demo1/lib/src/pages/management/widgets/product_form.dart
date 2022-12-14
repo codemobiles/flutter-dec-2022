90089219456321beab4fd117c4182efb75a17fca
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-14 14:18:37 +0700

    1234

--- demo1/lib/src/pages/management/widgets/product_form.dart
+++ demo1/lib/src/pages/management/widgets/product_form.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
 
+final formState = GlobalKey<FormState>();
+
 class ProductForm extends StatelessWidget {
   const ProductForm({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.all(20),
       child: Form(
+        key: formState,
         child: Column(
           children: [
-            Text("1234"),
-            Text("1234"),
+            TextFormField(
+              initialValue: "ProductA",
+              decoration: InputDecoration(labelText: "Name"),
+              onSaved: (value) {
+                print("Name: " + (value ?? ""));
+              },
+            ),
+            TextFormField(
+              initialValue: "100",
+              decoration: InputDecoration(labelText: "Price"),
+              onSaved: (value) {
+                print("Price: " + (value ?? ""));
+              },
+            ),
+            TextFormField(
+              initialValue: "1,000",
+              decoration: InputDecoration(labelText: "Name"),
+              onSaved: (value) {
+                print("Stock: " + (value ?? ""));
+              },
+            )
           ],
         ),
       ),

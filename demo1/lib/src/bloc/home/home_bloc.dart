58dafc06ea36516de71614ebc5de9ed1d65006f5
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-14 09:32:01 +0700

    1234

--- demo1/lib/src/bloc/home/home_bloc.dart
+++ demo1/lib/src/bloc/home/home_bloc.dart
@@ -3,24 +3,32 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:demo1/src/models/product.dart';
 import 'package:equatable/equatable.dart';
+
 part 'home_event.dart';
+
 part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   HomeBloc() : super(HomeState()) {
     // Add
     on<HomeEventAdd>((event, emit) {
-      emit(state.copyWith(count1: state.count1+1));
+      emit(state.copyWith(count1: state.count1 + 1));
     });
 
     // Remove
     on<HomeEventRemove>((event, emit) {
-      emit(state.copyWith(count1: state.count1-1));
+      emit(state.copyWith(count1: state.count1 - 1));
     });
 
     // Reset
     on<HomeEventReset>((event, emit) {
       emit(state.copyWith(count1: 0));
     });
+
+    // Load data
+    on<HomeEvent_Fetch>((event, emit) {});
+
+    // Toggle display mode (list and grid)
+    on<HomeEvent_toggleDisplay>((event, emit) {});
   }
 }

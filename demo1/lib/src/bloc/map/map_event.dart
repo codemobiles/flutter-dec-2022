2f9ab95ca86e8bed7f08b630c0faf0efbda9e361
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-15 14:54:12 +0700

    1234

--- demo1/lib/src/bloc/map/map_event.dart
+++ demo1/lib/src/bloc/map/map_event.dart
@@ -2,4 +2,12 @@ part of 'map_bloc.dart';
 
 abstract class MapEvent extends Equatable {
   const MapEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class MapEvent_SubmitLocation extends MapEvent{
+  final LatLng? position;
+  MapEvent_SubmitLocation({this.position});
 }

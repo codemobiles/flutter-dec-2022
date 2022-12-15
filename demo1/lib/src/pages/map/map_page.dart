30a75e5f9e0cf5fdcddd40ab56fe888f7b23c306
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-15 11:35:30 +0700

    1234

--- demo1/lib/src/pages/map/map_page.dart
+++ demo1/lib/src/pages/map/map_page.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
@@ -11,40 +12,29 @@ class MapPage extends StatefulWidget {
 }
 
 class MapPageState extends State<MapPage> {
-  final Completer<GoogleMapController> _controller =
-  Completer<GoogleMapController>();
-
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
+  static const CameraPosition _initMap = CameraPosition(
+    target: LatLng(13.7462463, 100.5325515),
+    zoom: 20,
   );
 
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  final Completer<GoogleMapController> _controller = Completer();
+  StreamSubscription<LocationData>? _locationSubscription;
+  final _locationService = Location();
+  final Set<Marker> _markers = {};
+  final Set<Polygon> _polygons = {};
+
+  final List<LatLng> _dummyLatLng = [
+    LatLng(13.729336912458525, 100.57422749698162),
+    LatLng(13.724325366482798, 100.58511726558208),
+    LatLng(13.716931129483003, 100.57489234954119),
+    LatLng(13.724794053328308, 100.56783076375723),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: const Text('To the lake!'),
-        icon: const Icon(Icons.directions_boat),
-      ),
+      body: Text("Map"),
     );
   }
 
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-  }
 }

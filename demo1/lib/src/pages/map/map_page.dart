937cefacd1e4b470fdcd7ae47b654b4663a1dc77
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-15 13:54:31 +0700

    1234

--- demo1/lib/src/pages/map/map_page.dart
+++ demo1/lib/src/pages/map/map_page.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:maps_toolkit/maps_toolkit.dart' as maptoolkit;
 
 import '../../services/common.dart';
 

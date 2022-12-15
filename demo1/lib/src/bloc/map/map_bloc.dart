ecec2f752f439737fc8428739c48db4ff28bc4d4
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-15 14:52:34 +0700

    1234

--- demo1/lib/src/bloc/map/map_bloc.dart
+++ demo1/lib/src/bloc/map/map_bloc.dart
@@ -5,10 +5,11 @@ import 'package:equatable/equatable.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 part 'map_event.dart';
+
 part 'map_state.dart';
 
 class MapBloc extends Bloc<MapEvent, MapState> {
-  MapBloc() : super(MapInitial()) {
+  MapBloc() : super(MapState(currentPosition: LatLng(13.7462463, 100.5325515))) {
     on<MapEvent>((event, emit) {
       // TODO: implement event handler
     });

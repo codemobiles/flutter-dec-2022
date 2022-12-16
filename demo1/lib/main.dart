54d4523d113ec969c269b2c54c007e7a5ec5ee39
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-16 14:04:44 +0700

    1234

--- demo1/lib/main.dart
+++ demo1/lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:demo1/src/app.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main(){
+Future<void> _messageHandler(RemoteMessage message) async {
+  print('background message ${message.notification!.body}');
+}
+
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_messageHandler);
+
   runApp(CMApp());
 }
